d7076b90660d4cec0a90776ae9daf45e9be267a9
ihebtmasek-bit <ihebtmasek@gmail.com>
2025-11-28 14:55:13 +0100

    Android Calculator Pro - API 34 Stable

---
+++ app/src/main/java/com/aap2calcul/app/MainActivity.java
@@ -0,0 +1,17 @@
+package com.aap2calcul.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/calculator.html");
+        setContentView(webView);
+    }
+}
